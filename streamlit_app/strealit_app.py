04f701b87a438555285bce445183691ec8416964
TimHealyIVEGroup <timothy.healy@ivegroup.com.au>
2026-07-03 14:16:41 +1000

    Update strealit_app.py

--- streamlit_app/strealit_app.py
+++ streamlit_app/strealit_app.py
@@ -42,7 +42,7 @@ if ingredients_list:
         
         st.subheader(fruit_chosen + ' Nutrition information')
         
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}" )  
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}" )  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
