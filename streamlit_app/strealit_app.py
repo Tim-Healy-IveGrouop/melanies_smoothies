813fc204ed2288ebbfab0137965a518604ddf152
TimHealyIVEGroup <timothy.healy@ivegroup.com.au>
2026-07-03 13:22:38 +1000

    Update strealit_app.py
    add requests

--- streamlit_app/strealit_app.py
+++ streamlit_app/strealit_app.py
@@ -2,6 +2,10 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
+
 
 # Write directly to the app
 st.title(":cup_with_straw: Customise Your Smoothie :cup_with_straw:")
@@ -14,7 +18,7 @@ name_on_order = st.text_input("Name on Smoothie")
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
     'Choose uo tp 5 ingredients:'
